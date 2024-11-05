87c0025f0af42d7ecb542ab637685a4da088efa1
Mihai Balica <balica@gmail.com>
2024-11-05 22:43:43 +0200

    docker and k8s - updates

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import os
 
 from pages.home_page import HomePage
 
@@ -20,15 +21,24 @@ def browser():
     options.add_argument("--no-sandbox")
     options.add_argument("--start-maximized")
     options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
 
     selenium_grid_url = "http://selenium-hub-service:4444/wd/hub"
-    ChromeDriverManager().install()
-    # service = Service(ChromeDriverManager().install())
-    driver = webdriver.Remote(command_executor=selenium_grid_url, options=options)
-    driver.implicitly_wait(30)
-    driver.maximize_window()
+    use_grid = os.getenv("USE_SELENIUM_GRID", "false").lower() == "true"
 
+    driver = None
+    driver = webdriver.Remote(
+        command_executor=selenium_grid_url, options=options
+    ) if use_grid else webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager().install()
+        ),
+        options=options
+    )
+
+    driver.implicitly_wait(30)
+    driver.set_page_load_timeout(30)
     yield driver
     driver.quit()
 
